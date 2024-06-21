85cbcec565ed88c198dcc11f82616d50036e2192
Michael1182mitnick <56063336+Michael1182mitnick@users.noreply.github.com>
2024-06-21 15:29:15 -0800

    Create test-repo2.py

---
+++ test-repo2.py
@@ -0,0 +1,7 @@
+# Subtracting two integer numbers
+first = int(input("Enter your first integer num: "))
+second = int(input("Enter your second integer num: "))
+# To subtract two numbers
+sub = first - second
+# To print and display the result
+print("The result of sub: " + str(sub))
